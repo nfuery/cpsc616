eb761bdfc96345136ab128449894df71c0d63121
Noah Fuery <nfuery@chapman.edu>
2023-09-18 23:36:18 -0700

    Updating and adding readme

---
+++ README.md
@@ -0,0 +1,12 @@
+Linux shell
+Noah Fuery
+CPSC616 - Professor Rene German
+9/18/2023
+
+Description:
+This program emulates the function of a linux terminal, taking in string input and then outputting the corresponding command based on the input. It uses a fork() method which creates a child process, in the child process the command entered by the user is exectued and then is displayed. The program accounts for a simple level of error handling.
+
+Sources:
+stackoverflow.com
+geeksforgeeks.com
+github.com
--- shell.cpp
+++ shell.cpp
@@ -1,28 +1,34 @@
-#include <sys/wait.h>
-#include <unistd.h>
-#include <iostream>
-#include <string>
+#include <sys/wait.h>   // Header for wait functions
+#include <unistd.h>     // Header for fork and execlp functions
+#include <iostream>     
+#include <string>       
 
-using namespace std;
+using namespace std;    
 
 int main() {
-    string command;
-    while (true) {
-        cout << "Enter command: ";
-        getline(cin, command);
-        if (command == "exit") {
+    string command;     // Declare a string variable to store user input
+
+    while (true) {      // Start loop
+        cout << "Enter command: ";  // Prompt the user to enter a command
+        getline(cin, command);      // Read a line of input from the user and store it in 'command'
+
+        if (command == "exit") {    // If the user enters "exit," exit the loop
             break;
         }
-        pid_t pid = fork();
-        if (pid == 0) {
-            execlp(command.c_str(), command.c_str(), NULL);
-            exit(EXIT_FAILURE);
-        } else if (pid > 0) {
-            wait(NULL);
-        } else {
-            cerr << "Fork failed" << endl;
-            return 1;
+
+        pid_t pid = fork();         // Create a child process using 'fork'
+
+        if (pid == 0) {             // In the child process
+            execlp(command.c_str(), command.c_str(), NULL);  // Execute the command entered by the user
+            exit(EXIT_FAILURE);     // If execlp fails, exit the child process with a failure status
+        } else if (pid > 0) {        // In the parent process
+            wait(NULL);             // Wait for the child process to complete
+            cout << "Unknown command" << endl;  // Display a message indicating an unknown command
+        } else {                     // If 'fork' fails to create a child process
+            cerr << "Fork failed" << endl;  // Display an error message
+            return 1;              // Exit the program with an error status
         }
     }
-    return 0;
+
+    return 0;  // Exit the program with a success status
 }
