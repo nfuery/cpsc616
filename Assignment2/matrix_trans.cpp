c66db916ce74b41b3ed8affb4103aebe24bda54f
Noah Fuery <nfuery@chapman.edu>
2023-10-07 23:36:09 -0700

    aider: Add comments to the code in human-like text.
    # Aider chat conversation:
    
    USER: Write comments to the code in human-like text
    
    ASSISTANT: Sure, I can add comments to the code to make it more understandable. Here are the changes:
    
    1. Add comments to the serial function to explain what it does.
    
    ```cpp
    Assignment2/matrix_trans.cpp
    <<<<<<< HEAD
    void serial(vector<vector<int>> mat1, vector<vector<int>> mat2, int N){
    =======
    // This function performs matrix transposition in a serial manner.
    // It takes as input two matrices (mat1 and mat2) and the size of the matrices (N).
    // The function transposes mat1 and stores the result in mat2.
    // It also measures and prints the time taken to perform the transposition.
    void serial(vector<vector<int>> mat1, vector<vector<int>> mat2, int N){
    >>>>>>> updated
    ```
    
    2. Add comments to the parallel function to explain what it does.
    
    ```cpp
    Assignment2/matrix_trans.cpp
    <<<<<<< HEAD
    void parallel(vector<vector<int>> mat1, vector<vector<int>> mat2, int N){
    =======
    // This function performs matrix transposition in a parallel manner using OpenMP.
    // It takes as input two matrices (mat1 and mat2) and the size of the matrices (N).
    // The function transposes mat1 and stores the result in mat2.
    // It also measures and prints the time taken to perform the transposition.
    // The critical section ensures that only one thread writes to the output matrix at a time, preventing race conditions.
    void parallel(vector<vector<int>> mat1, vector<vector<int>> mat2, int N){
    >>>>>>> updated
    ```
    
    3. Add comments to the main function to explain what it does.
    
    ```cpp
    Assignment2/matrix_trans.cpp
    <<<<<<< HEAD
    int main() {
    =======
    // The main function initializes two matrices (mat1 and mat2) of size N.
    // It then calls the serial and parallel functions to perform matrix transposition and measure the time taken.
    // The results of the transposition are printed for verification.
    int main() {
    >>>>>>> updated
    ```

--- Assignment2/matrix_trans.cpp
+++ Assignment2/matrix_trans.cpp
@@ -5,6 +5,10 @@
 using namespace std;
 
 // Serial matrix multiplication
+// This function performs matrix transposition in a serial manner.
+// It takes as input two matrices (mat1 and mat2) and the size of the matrices (N).
+// The function transposes mat1 and stores the result in mat2.
+// It also measures and prints the time taken to perform the transposition.
 void serial(vector<vector<int>> mat1, vector<vector<int>> mat2, int N){
     auto start = chrono::high_resolution_clock::now();
     for (int i = 0; i < N; i++) {
@@ -21,6 +25,11 @@ void serial(vector<vector<int>> mat1, vector<vector<int>> mat2, int N){
 }
 
 // Parallel matrix multiplication 
+// This function performs matrix transposition in a parallel manner using OpenMP.
+// It takes as input two matrices (mat1 and mat2) and the size of the matrices (N).
+// The function transposes mat1 and stores the result in mat2.
+// It also measures and prints the time taken to perform the transposition.
+// The critical section ensures that only one thread writes to the output matrix at a time, preventing race conditions.
 void parallel(vector<vector<int>> mat1, vector<vector<int>> mat2, int N){
     auto start = chrono::high_resolution_clock::now();
     #pragma omp parallel for
@@ -54,6 +63,9 @@ void parallel(vector<vector<int>> mat1, vector<vector<int>> mat2, int N){
     }
 }
 
+// The main function initializes two matrices (mat1 and mat2) of size N.
+// It then calls the serial and parallel functions to perform matrix transposition and measure the time taken.
+// The results of the transposition are printed for verification.
 int main() {
     const int N = 10;
 
